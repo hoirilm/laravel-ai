1749a3ebf9abe7b08ae09996973f9340ad9b54f4
hoiril.mochtar <hoiril.mochtar@dwidasa.com>
2026-06-27 12:53:44 +0700

    feat: add command to run Laravel dev server and Vite HMR concurrently

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,27 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Process\Process;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('dev', function () {
+    $this->info('Starting Laravel dev server and Vite...');
+
+    $process = new Process([
+        'npx', 'concurrently',
+        '-c', '#93c5fd,#c4b5fd',
+        '--names', 'server,vite',
+        '--kill-others',
+        'php artisan serve',
+        'npm run dev',
+    ]);
+
+    $process->setTimeout(null);
+    $process->setTty(true);
+    $process->run(function ($type, $buffer) {
+        echo $buffer;
+    });
+})->purpose('Run Laravel dev server and Vite HMR concurrently');
